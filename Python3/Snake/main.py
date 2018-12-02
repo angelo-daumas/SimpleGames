7cc4b480bd89b467a2ec12dd2bcb56345d440de4
Ângelo Daumas <42705006+angelo-daumas@users.noreply.github.com>
2018-12-02 02:32:56 -0200

    Cleaned up movement method

--- Python3/Snake/main.py
+++ Python3/Snake/main.py
@@ -8,8 +8,8 @@ class cobrinha():
               def random(self):
                      xi = randint(1,len(self))
                      self.rotate(xi)
-                     return self.pop() 
-              
+                     return self.pop()
+
        class direction(list):
 
               @property
@@ -27,13 +27,13 @@ class cobrinha():
               @y.setter
               def y(self, value):
                      self[1] = value
-       
-              
+
+
 
               def __mul__(self, number):
                      return (self[0]*number, self[1]*number)
-                     
-       
+
+
        def __init__(self,master):
               self.master = master
               self.width = 502
@@ -51,7 +51,7 @@ class cobrinha():
                                                         22,22,
                                                         fill = '#0066FF')
               self.cobra.append(self.cabeca)
-              
+
               self.direcao = type(self).direction((1, 0))
               self.aumentar()
               self.aumentar()
@@ -71,19 +71,19 @@ class cobrinha():
                      self.direcao.x = 1
                      self.direcao.y = 0
                      self.turned = True
-                     
+
        def esquerda(self,evento):
               if self.direcao.x == 0 and not self.turned:
                      self.direcao.x = -1
                      self.direcao.y = 0
                      self.turned = True
-                
+
        def cima(self,evento):
               if self.direcao.y == 0 and not self.turned:
                      self.direcao.x = 0
                      self.direcao.y = -1
                      self.turned = True
-                     
+
        def baixo(self,evento):
               if self.direcao.y == 0 and not self.turned:
                      self.direcao.x = 0
@@ -94,66 +94,61 @@ class cobrinha():
               raio = 5
               xposicoes = type(self).deque(range(7,490,20))
               yposicoes = type(self).deque(range(7,490,20))
-              
+
               xp = xposicoes.random()
               yp = yposicoes.random()
-              
+
               while(self.canvas.find_overlapping(xp, xp+1, yp, yp+1)):
                      xp = xposicoes.random()
                      yp = yposicoes.random()
-                     
+
 
 
               self.comida = self.canvas.create_oval(xp,yp,
                                                     2*raio+xp,
                                                     2*raio+yp,
                                                     fill = '#00FFFF')
-              
-                                                    
+
+
        def aumentar(self):
               x1,y1,x2,y2 = self.canvas.coords(self.cobra[-1])
 
               x1 = x1-self.direcao.x*20
               x2 = x2-self.direcao.x*20
-              
+
               seg = self.canvas.create_rectangle(x1,y1,
                                                  x2,y2,
                                                  fill = '#000066')
               self.cobra.append(seg)
 
        def mover(self):
-              x1,y1,x2,y2 = self.canvas.coords(self.cabeca)
+
+              for i in range(len(self.cobra)-1, 0, -1):
+                               self.canvas.coords(self.cobra[i], *self.canvas.coords(self.cobra[i-1]))
 
               
               self.canvas.move(self.cabeca, *self.direcao*20)
-              self.turned = False
-              
-              self.comer()
-              for segmento in self.cobra[1:]:
-                     x3,y3,x4,y4 = self.canvas.coords(segmento)
-                     self.canvas.coords(segmento,\
-                                        x1,y1,x2,y2)
-                     x1,y1,x2,y2 = x3,y3,x4,y4
-
               x1,y1,x2,y2 = self.canvas.coords(self.cabeca)
+              self.turned = False
+              self.comer()
+
               if x2 > self.width:
                      x1 = 2
                      x2 = 22
-              if x1 < 2: 
+              if x1 < 2:
                      x1 = self.width - 20
                      x2 = self.width
               if y2 > self.width:
                      y1 = 2
                      y2 = 22
-              if y1 < 2: 
+              if y1 < 2:
                      y1 = self.width - 20
                      y2 = self.width
 
               self.canvas.coords(self.cabeca,x1,y1,x2,y2)
-              if (len(self.canvas.find_overlapping(x1+1,y1+1,x2-1,y2-1))) > 1:
-                     return 0
-              return 1
-              
+              return not len(self.canvas.find_overlapping(x1+1,y1+1,x2-1,y2-1)) > 1
+
+
 
        def comer(self):
               x1cab,y1cab,x2cab,y2cab = self.canvas.coords(self.cabeca)
@@ -172,10 +167,10 @@ class cobrinha():
                                              fill="#0066FF",
                                              font="Times 20 italic bold",
                                              text="Game over.")
-              
-              
-                   
-              
+
+
+
+
 root = Tk()
 jogo = cobrinha(root)
 jogo.jogo()
